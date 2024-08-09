97122626c0384c5c6d01b7246ff6a21b8f8664f8
Mahmut İÇME <arrovain@gmail.com>
2024-08-09 09:32:04 +0300

    route for admin/login with AdminController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('admin/login', [AdminController::class, 'index'])->name('admin.login');
