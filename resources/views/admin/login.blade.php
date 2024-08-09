1c2cc7dcb08e582f58917c95b974f443344eb62b
Mahmut İÇME <arrovain@gmail.com>
2024-08-09 11:17:52 +0300

    post - admin authenticate

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -25,9 +25,9 @@
 </div>
 <div class="card-body">
 <p class="login-box-msg">Sign in to start your session</p>
-<form action="https://adminlte.io/themes/v3/index3.html" method="post">
+<form action="{{route('admin.authenticate')}}" method="post">
 <div class="input-group mb-3">
-<input type="email" class="form-control" placeholder="Email">
+<input type="email" name="email" class="form-control" placeholder="Email">
 <div class="input-group-append">
 <div class="input-group-text">
 <span class="fas fa-envelope"></span>
@@ -35,7 +35,7 @@
 </div>
 </div>
 <div class="input-group mb-3">
-<input type="password" class="form-control" placeholder="Password">
+<input type="password" name="password" class="form-control" placeholder="Password">
 <div class="input-group-append">
 <div class="input-group-text">
 <span class="fas fa-lock"></span>
