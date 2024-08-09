cf99db87730c031e80eb15358f9d239d79cd89d3
Mahmut İÇME <arrovain@gmail.com>
2024-08-09 15:31:34 +0300

    group all routes

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::get('admin/login', [AdminController::class, 'index'])->name('admin.login');
-Route::get('admin/logout', [AdminController::class, 'logout'])->name('admin.logout');
+Route::group(['prefix'=>'admin'], function(){
+    Route::group(['middleware'=>'admin.guest'],function(){
+        Route::get('admin/login', [AdminController::class, 'index'])->name('admin.login');
 Route::get('admin/register', [AdminController::class, 'register'])->name('admin.register');
 Route::post('admin/login', [AdminController::class, 'authenticate'])->name('admin.authenticate');
-Route::get('admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+
+    });
+    Route::group(['middleware'=>'admin.auth'],function(){
+        Route::get('admin/logout', [AdminController::class, 'logout'])->name('admin.logout');
+        Route::get('admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
 Route::get('admin/form', [AdminController::class, 'form'])->name('admin.form');
 Route::get('admin/table', [AdminController::class, 'table'])->name('admin.table');
+    });
+    
+    
+});
+
