aa224e6f1396277c203aef045a5f44f9e2a58f25
Mahmut İÇME <arrovain@gmail.com>
2024-08-12 14:00:42 +0300

    route and index function for feehead

--- app/Http/Controllers/FeeHeadController.php
+++ app/Http/Controllers/FeeHeadController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\FeeHead;
 use Illuminate\Http\Request;
+
 
 class FeeHeadController extends Controller
 {
-    //
+    public function index(){
+        return view('admin.fee-head.fee-head');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AcademicYearController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ClassesController;
+use App\Http\Controllers\FeeHeadController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -16,7 +17,6 @@ Route::group(['prefix'=>'admin'], function(){
 Route::get('register', [AdminController::class, 'register'])->name('admin.register');
 Route::post('login', [AdminController::class, 'authenticate'])->name('admin.authenticate');
 
-    });
     Route::group(['middleware'=>'admin.auth'],function(){
         Route::get('logout', [AdminController::class, 'logout'])->name('admin.logout');
         Route::get('dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
@@ -36,7 +36,10 @@ Route::get('class/read', [ClassesController::class, 'read'])->name('class.read')
 Route::get('class/edit{id}', [ClassesController::class, 'edit'])->name('class.edit');
 Route::post('class/update', [ClassesController::class, 'update'])->name('class.update');
 Route::get('class/delete{id}', [ClassesController::class, 'delete'])->name('class.delete');
-    });
+
+
+Route::get('fee-head/create', [FeeHeadController::class, 'index'])->name('fee-head.create');
+
     
     
 });
