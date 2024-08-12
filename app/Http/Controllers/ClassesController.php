54a784e90456a570cc302f1b70e4454192aecfa5
Mahmut İÇME <arrovain@gmail.com>
2024-08-12 09:50:22 +0300

    store function

--- app/Http/Controllers/ClassesController.php
+++ app/Http/Controllers/ClassesController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Classes;
+
+
 use Illuminate\Http\Request;
 
 class ClassesController extends Controller
@@ -9,4 +12,16 @@ class ClassesController extends Controller
     public function index (){
         return view('admin.class.class');
     }
+
+    
+ public function store(Request $request){
+    $request->validate([
+        'name' => 'required'
+    ]);
+    $data = new Classes();
+    $data -> name = $request->name;
+    $data->save();
+    return redirect()->route('class.create')->with('success', 'Class added successfully' );
+ }
 }
+
