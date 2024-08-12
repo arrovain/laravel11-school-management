5c96b2341ab237f3ed40a1db84cd88ddceb0d24f
Mahmut İÇME <arrovain@gmail.com>
2024-08-12 10:07:16 +0300

    edit function (Classes::find($id)

--- app/Http/Controllers/ClassesController.php
+++ app/Http/Controllers/ClassesController.php
@@ -23,5 +23,15 @@ class ClassesController extends Controller
     $data->save();
     return redirect()->route('class.create')->with('success', 'Class added successfully' );
  }
+
+ public function read (){
+    return view('admin.class.class_list');
+}
+
+public function edit ($id){
+    $data['class'] = Classes::find($id);
+    return view('admin.class.edit_class', $data);
+}
+
 }
 
